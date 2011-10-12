f2e7595e148b3d4b57ea8573a328e507ab8be500
nathanvda <nathan@dixis.com>
2011-10-12 23:08:20 +0200

    Use a callback on insertion and removal of a belongs_to relation.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,17 @@
 
 $(document).ready(function() {
     $("#owner a.add_fields").
-      data("insertion-callback",
+      data("association-insertion-position", 'before').
+      data("association-insertion-node", 'this');
+
+    $('#owner').bind('insertion-callback',
          function() {
            $("#owner_from_list").hide();
            $("#owner a.add_fields").hide();
+         });
+    $('#owner').bind("removal-callback",
+         function() {
+           $("#owner_from_list").show();
+           $("#owner a.add_fields").show();
          });
 });
