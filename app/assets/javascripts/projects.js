da15a27026f8ef04cd21c18cdc5c993fab14c3dd
nathanvda <nathan@dixis.com>
2011-10-22 23:14:55 +0200

    Clean up styling and functionality.

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,8 +1,33 @@
 $(document).ready(function() {
     $("#owner a.add_fields").
-      data("insertion-callback",
+      data("association-insertion-position", 'before').
+      data("association-insertion-node", 'this');
+
+    $('#owner').bind('insertion-callback',
          function() {
-           alert('link clicked');
-           $("owner_from_list").hide();
+           $("#owner_from_list").hide();
+           $("#owner a.add_fields").hide();
          });
+    $('#owner').bind("removal-callback",
+         function() {
+           $("#owner_from_list").show();
+           $("#owner a.add_fields").show();
+         });
+
+    $("#tags a.add_fields").
+      data("association-insertion-position", 'before').
+      data("association-insertion-node", 'this');
+
+    $('#tags').bind('insertion-callback',
+         function() {
+             $(".project-tag-fields a.add_fields").
+                 data("association-insertion-position", 'before').
+                 data("association-insertion-node", 'this');
+             $('.project-tag-fields').bind('insertion-callback',
+                  function() {
+                    $(this).children("#tag_from_list").remove();
+                    $(this).children("a.add_fields").hide();
+                  });
+         });
+
 });
