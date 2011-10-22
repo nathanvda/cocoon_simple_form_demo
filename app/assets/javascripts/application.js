cfb05acaf57776d343043a12a2e3bf5f5618cc59
nathanvda <nathan@dixis.com>
2011-10-22 23:14:19 +0200

    Removed all js from application.js and moved to projects.js.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,19 +10,3 @@
 //= require_tree .
 
 
-$(document).ready(function() {
-    $("#owner a.add_fields").
-      data("association-insertion-position", 'before').
-      data("association-insertion-node", 'this');
-
-    $('#owner').bind('insertion-callback',
-         function() {
-           $("#owner_from_list").hide();
-           $("#owner a.add_fields").hide();
-         });
-    $('#owner').bind("removal-callback",
-         function() {
-           $("#owner_from_list").show();
-           $("#owner a.add_fields").show();
-         });
-});
