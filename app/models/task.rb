6ef1a3ad98db62f635165fa4cb159d2569ed3565
nathanvda <nathan@dixis.com>
2017-01-15 17:24:43 +0100

    Add validation for test
    To enable testing cocoon #405

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ActiveRecord::Base
   has_many :sub_tasks
   accepts_nested_attributes_for :sub_tasks, :reject_if => :all_blank, :allow_destroy => true
+
+  validates_presence_of :name
 end
